5ce317ecebc0a5d52eb3bc86bf5edbe2b660d509
jerluc <me@jerluc.com>
2023-07-27 23:08:42 -0700

    [config] Adds editorconfig

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-set -xe
+set -e
 
 polka_dir=$(dirname $(realpath $0))
-configs=$(ls $polka_dir/overlays/home/.config/)
 
-for config_dir in $configs; do
-    ln -s $polka_dir/overlays/home/.config/$config_dir $HOME/.config/$config_dir
+maybe_setup_link () {
+    src=$1
+    dst=$2
+    if [ ! -h "$dst" ]; then
+        ln -s $src $dst
+        echo "Created symlink: $dst"
+    fi
+}
+
+maybe_setup_link $polka_dir/overlays/home/.editorconfig $HOME/.editorconfig
+for config_dir in $(ls $polka_dir/overlays/home/.config/); do
+    maybe_setup_link $polka_dir/overlays/home/.config/$config_dir $HOME/.config/$config_dir
 done
