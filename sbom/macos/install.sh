0cf73b7f56aa1532788cad8c7ecabdc30bf8b280
jerluc <me@jerluc.com>
2023-07-27 22:54:16 -0700

    [sbom] macOS updates

--- sbom/macos/install.sh
+++ sbom/macos/install.sh
@@ -1,18 +1,69 @@
 #!/usr/bin/env bash
 
 
-### <FONTS> ###
+# Taps
 
-# Hack
 brew tap homebrew/cask-fonts
-brew install --cask font-hack-nerd-font
-
-# SF Mono + ligatures
 brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
+
+
+# Casks
+
+brew install --cask 1password-cli
+brew install --cask discord
+brew install --cask font-hack-nerd-font
 brew install --cask font-sf-mono-nerd-font-ligaturized
+brew install --cask font-source-code-pro-for-powerline
+brew install --cask sf-symbols
 
-# Sketchybar supporting fonts
+# Formulae
 
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.4/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-
-### </FONTS> ###
+brew install asciinema
+brew install atlas
+brew install bash
+brew install binutils
+brew install coreutils
+brew install curl
+brew install editorconfig
+brew install flux
+brew install gh
+brew install git
+brew install gnupg
+brew install go
+brew install helm
+brew install htop
+brew install imagemagick
+brew install jq
+brew install k9s
+brew install kakoune
+brew install kubeseal
+brew install libmagic
+brew install libsodium
+brew install llvm
+brew install lua
+brew install luajit
+brew install lz4
+brew install lzo
+brew install make
+brew install mosh
+brew install neofetch
+brew install nnn
+brew install openjdk
+brew install pipx
+brew install podman
+brew install postgresql
+brew install protobuf
+brew install pyenv
+brew install qemu
+brew install ripgrep
+brew install skhd
+brew install sops
+brew install sqlite
+brew install starship
+brew install subversion
+brew install tig
+brew install vim
+brew install watch
+brew install wget
+brew install wrk
+brew install yabai
