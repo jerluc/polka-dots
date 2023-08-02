a7da6cd22be40441fee9505aac0c8c7fd0e5285c
Jeremy Lucas <me@jerluc.com>
2023-08-01 18:15:33 -0700

    [setup] Adding simple patch capabilities
    Also removes link-based backend

--- setup.sh
+++ setup.sh
@@ -3,22 +3,63 @@
 set -e
 
 polka_dir=$(dirname $(realpath $0))
+machine=$(hostname)
+version=$(date -u '+%s')
 
-maybe_setup_link () {
-    src=$1
-    dst=$2
+maybe_update () {
+    home_relative_file=$1
+    src="$polka_dir/overlays/home/$home_relative_file"
+    dst="$HOME/$home_relative_file"
 
-    if [[ ! -h "$dst" ]]; then
-        if [[ -e "$dst" ]]; then
-            echo "Cowardly refusing to update path: $dst"
+    if [[ -e "$dst" ]]; then
+        read -p "Overwrite local file '$dst'? [Y/n]: " -n 1 -r
+        echo    # (optional) move to a new line
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            do_update $home_relative_file
         else
-            ln -s $src $dst
-            echo "Created symlink: $dst"
+            echo "Skipping update for: $dst"
         fi
+    else
+        do_update $home_relative_file
     fi
 }
 
-maybe_setup_link $polka_dir/overlays/home/.editorconfig $HOME/.editorconfig
+do_update () {
+    home_relative_file=$1
+    src="$polka_dir/overlays/home/$home_relative_file"
+    dst="$HOME/$home_relative_file"
+
+    cp $dst "$dst.$version.bak"
+    cp $src $dst
+    echo "- Updated file: $dst"
+
+    maybe_patch $home_relative_file
+}
+
+maybe_patch () {
+    home_relative_file=$1
+    src="$polka_dir/patches/home/$home_relative_file"
+    dst="$HOME/$home_relative_file"
+
+    patch_file="$src.$machine.patch"
+    if [[ -e "$patch_file" ]]; then
+        patch -s $dst $patch_file
+        echo "- Applied patch for: $dst"
+    fi
+}
+
+maybe_update .editorconfig
 for config_dir in $(ls $polka_dir/overlays/home/.config/); do
-    maybe_setup_link $polka_dir/overlays/home/.config/$config_dir $HOME/.config/$config_dir
+    for config_file in $(ls $polka_dir/overlays/home/.config/$config_dir); do
+        home_relative_dir=.config/$config_dir
+        home_relative_file=$home_relative_dir/$config_file
+
+        # NOTE: For now, only overwrite files
+        if [[ -f "$polka_dir/overlays/home/$home_relative_file" ]]; then
+            # TODO: Add a way to force create the directory?
+            if [[ -d "$HOME/$home_relative_dir" ]]; then
+                maybe_update $home_relative_file
+            fi
+        fi
+    done
 done
